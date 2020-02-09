81e57c8c36c42576a0b6760a71eb0bcd8788f5d9
刘大小 <33533223+growvv@users.noreply.github.com>
2020-02-09 14:00:10 +0800

    Create md2html.js

---
+++ md2html.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const marked = require('marked')
+
+
+fs.readFile('./titles.md', 'utf-8', (err, data)=>{
+    if(err){
+        throw err
+    }else{
+        // 3.使用marked方法，将md格式的文件转化为html格式
+        let htmlStr = marked(data.toString());
+        // 4.将转化的html格式的字符串，写入到新的文件中
+        fs.writeFile('./titles.html', htmlStr, err=>{
+            if(err){
+                throw err
+            }else{
+                console.log("success")
+            }
+        })
+    }
+})
