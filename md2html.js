df854f6cfbe6ff452232eb5a9abb5b62e33ff633
刘大小 <33533223+growvv@users.noreply.github.com>
2020-02-09 17:16:40 +0800

    Update md2html.js

--- md2html.js
+++ md2html.js
@@ -19,3 +19,21 @@ fs.readFile('./titles.md', 'utf-8', (err, data)=>{
         })
     }
 })
+
+fs.readFile('./record.md', 'utf-8', (err, data)=>{
+    if(err){
+        throw err
+    }else{
+        // 3.使用marked方法，将md格式的文件转化为html格式
+        let htmlStr = marked(data.toString());
+        // 4.将转化的html格式的字符串，写入到新的文件中
+        fs.writeFile('./record.html', htmlStr, err=>{
+            if(err){
+                throw err
+            }else{
+                console.log("success")
+                console.log(htmlStr)
+            }
+        })
+    }
+})
