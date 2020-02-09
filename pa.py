de38cb79ebe3db945fd4e95b0c3fbfe044420aa6
刘大小 <33533223+growvv@users.noreply.github.com>
2020-02-09 14:45:16 +0800

    Update pa.py

--- pa.py
+++ pa.py
@@ -12,12 +12,12 @@ def get_title(url):
     print(len(titles))
     with open('titles.md', 'a', encoding="utf-8") as f:  #使用utf-8编码
         for i, title in enumerate(titles):
-            s = f'{id} [{title.text}]({title.attrs["href"]})'
+            s = f'{id}. [{title.text}]({title.attrs["href"]})'
             print(s)
             f.write(s + '\n')
             id = id + 1
 
 if __name__ == '__main__':
-    for x in range(1, 3):
+    for x in range(1, 10):
         print('当前页面: '+ str(x))
         get_title(base_url+str(x))
